784844c4dbdbf4428ed0acc17be291747677eb9d
Siim B <m2ngija@gmail.com>
2020-03-10 11:45:04 +0200

    idk

--- .config/scripts/cc.sh
+++ .config/scripts/cc.sh
@@ -1,5 +1,4 @@
 cp ~/.xinitrc ~/Downloads/config/
 cp ~/.zshrc ~/Downloads/config/
-cp -r ~/.config/scripts ~/.config/kitty ~/.config/mpv ~/.config/ncmpcpp ~/.config/ranger ~/.config/sxhkd ~/Downloads/config/.config
-cp -r ~/.config/mpd/database ~/.config/mpd/playlists ~/Downloads/config/.config/mpd
+cp -r ~/.config/scripts ~/.config/kitty ~/.config/mpd ~/.config/mpv ~/.config/ncmpcpp ~/.config/ranger ~/.config/sxhkd ~/Downloads/config/.config
 cp ~/.config/nvim/coc.vim ~/.config/nvim/coc-settings.json ~/.config/nvim/init.vim ~/.config/nvim/nerdtree.vim ~/.config/nvim/plugins.vim ~/Downloads/config/.config/nvim
